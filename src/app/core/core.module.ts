a6f7e3eecba4d072a750ea0ea6d84feef2609bb4
MajaZarkova <majazarkova@gmail.com>
2022-04-14 18:17:11 +0300

    Responsive navigation with animation added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,14 @@ import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { errorInterceptor } from './error-handler';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
@@ -14,7 +22,14 @@ import { errorInterceptor } from './error-handler';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   exports: [
     HeaderComponent,
